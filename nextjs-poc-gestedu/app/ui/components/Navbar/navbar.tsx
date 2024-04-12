1addbdc06fd80542ff37fb93d0e63988852d4907
Maria Rita Iglesias <rita.iglesias.adrover@gmail.com>
2024-04-12 19:38:02 -0300

    Setting ignore case false

---
+++ nextjs-poc-gestedu/app/ui/components/Navbar/navbar.tsx
@@ -0,0 +1,27 @@
+'use client';
+import styles from './navbar.module.css';
+import GestEduIcon from '@/ui/assets/svg/logo-black-horizontal.svg';
+import Login from '@/ui/assets/svg/login.svg';
+import Link from 'next/link';
+import { useContext } from 'react';
+import { LoginModalContext } from 'context';
+
+export default function Navbar() {
+  const loginModalContext = useContext(LoginModalContext);
+  function handleClick() {
+    loginModalContext?.setOpen(true);
+  }
+  return (
+    <nav className={styles.navbar}>
+      <Link href='/'>
+        <GestEduIcon />
+      </Link>
+      {!loginModalContext?.open && (
+        <button className={styles.signin} onClick={handleClick}>
+          <Login />
+          <span>Ingresar</span>
+        </button>
+      )}
+    </nav>
+  );
+}
